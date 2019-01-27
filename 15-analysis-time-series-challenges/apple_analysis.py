e909c7794bef0743140d994cc41aca1f90867ca1
code <42758914+chenanming@users.noreply.github.com>
2019-01-27 14:04:01 +0800

    Update apple_analysis.py

--- 15-analysis-time-series-challenges/apple_analysis.py
+++ 15-analysis-time-series-challenges/apple_analysis.py
@@ -4,6 +4,11 @@ import pandas as pd
 
 def quarter_volume():
     df = pd.read_csv('apple.csv',header=0)
+    '''
+    1.使用pandas选择数据Volume
+    2.转换为时间序列
+    3.按季度重新采样并排序
+    '''
     a = df.Volume
     a.index = pd.to_datetime(df.Date)
     second_volume = a.resample('Q').sum().sort_values()[-2]
