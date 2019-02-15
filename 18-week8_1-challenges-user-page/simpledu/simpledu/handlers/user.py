7b578ea2672cd98e7bcf2fee92e10697613210f6
code <42758914+chenanming@users.noreply.github.com>
2019-02-15 13:51:05 +0800

    Update user.py

--- 18-week8_1-challenges-user-page/simpledu/simpledu/handlers/user.py
+++ 18-week8_1-challenges-user-page/simpledu/simpledu/handlers/user.py
@@ -1,11 +1,10 @@
 from flask import Blurprint, render_template
-from simpledu.models import Course
+from simpledu.models import User
 
 
 user = Blurprint('user', __name__, url_prefix='/user')
 
-@user.route('/')
-def user_index():
-    username 
-    courses = Course.query.all()
-    return render_templedu('user/datatil.html', courses=courses)
+@user.route('/<username>')
+def index(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user/detail.html', user=user)
